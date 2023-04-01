b4454d7d35201ca11cbc59dac204b02c0e97d46b
Sophie Tsai <67646012+sophtsai@users.noreply.github.com>
2023-04-01 18:54:38 +0000

    Add style for dark theme and change theme toggle switch

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let themeButton = document.getElementById("theme-button");
 
 const toggleDarkMode = () => {
-  document.body.classList.toggle("dark-main")
-  // document.getElementById("intro-section").toggle("section-dark")
+  document.body.classList.toggle("light-main");  
 }
 
-themeButton.addEventListener("click", toggleDarkMode);
+// themeButton.addEventListener("click", toggleDarkMode);
+
+themeButton.addEventListener("change", toggleDarkMode);

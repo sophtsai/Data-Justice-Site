e8d274e99db3ba392b83221c6e342467d162bbe1
Sophie Tsai <67646012+sophtsai@users.noreply.github.com>
2023-04-02 06:10:55 +0000

    Add petition form

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Theme toggle functionality
 let themeButton = document.getElementById("theme-button");
 
 const toggleDarkMode = () => {
@@ -5,3 +6,21 @@ const toggleDarkMode = () => {
 }
 
 themeButton.addEventListener("change", toggleDarkMode);
+
+// Petition Form functionality
+let signNowButton = document.querySelector("#sign-now-button");
+
+const addSignature = () => {
+  let name = document.getElementById('name').value;
+  let hometown = document.getElementById('hometown').value;
+  let email = document.getElementById('email').value;
+
+  let supportSent = document.createElement("p");
+  supportSent.innerHTML = "🖊️ " + name + " from " + hometown + " supports this."
+
+  let supportList = document.querySelector(".signatures");
+  supportList.appendChild(supportSent);
+}
+
+signNowButton.addEventListener("click", addSignature);
+

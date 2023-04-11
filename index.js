dad87a822d38f38096bb020341f96995d5468880
Sophie Tsai <>
2023-04-11 14:33:31 +0000

    Add form functionality

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 let themeButton = document.getElementById("theme-button");
 
 const toggleDarkMode = () => {
-  document.body.classList.toggle("light-main");  
+  document.body.classList.toggle("light-main");
 }
 
 themeButton.addEventListener("change", toggleDarkMode);
 
-// Petition Form functionality
+// Petition form functionality
 let signNowButton = document.querySelector("#sign-now-button");
+let count = 3;
 
 const addSignature = () => {
   let name = document.getElementById('name').value;
@@ -20,7 +21,55 @@ const addSignature = () => {
 
   let supportList = document.querySelector(".signatures");
   supportList.appendChild(supportSent);
+
+  document.getElementById('counter').remove();
+  count = count + 1;
+
+  let sigCount = document.createElement("p");
+  sigCount.innerHTML = "🖊️ " + count + " people have signed this petition and support this cause.";
+  sigCount.setAttribute('id', 'counter');
+
+  let sigCountShow = document.querySelector(".countShow");
+  sigCountShow.appendChild(sigCount);
+
 }
 
-signNowButton.addEventListener("click", addSignature);
+// Petition form validation functionality
+const validateForm = () => {
 
+  let containsErrors = false;
+
+  var petitionInputs = document.getElementById("sign-petition").elements;
+
+  // Check if any input is less than 2 characters
+  for (let i = 0; i < petitionInputs.length; i++) {
+    if (petitionInputs[i].value.length < 2) {
+      containsErrors = true;
+      petitionInputs[i].classList.add('error');
+    } else {
+      petitionInputs[i].classList.remove('error');
+    }
+  }
+
+  // Check if email input consists of .com
+  const email = document.getElementById('email');
+  if (!email.value.includes('.com')) {
+    containsErrors = true;
+    email.classList.add('error');
+  } else {
+    email.classList.remove('error');
+  }
+
+  // Add signature and reset validation
+  if (containsErrors == false) {
+    addSignature();
+    for (let i = 0; i < petitionInputs.length; i++) {
+      petitionInputs[i].value = "";
+      containsErrors = false;
+    }
+  }
+
+}
+
+
+signNowButton.addEventListener('click', validateForm);
